25b32c221f489606779e252537de5a39924fd9d4
Arnab Biswas <abiswas@commvault.com>
2022-07-09 18:28:25 +0530

    Test Changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Just random testing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome To Influencer Management Portal/);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/HomePage/HomPage.js
+++ src/components/HomePage/HomPage.js
@@ -3,7 +3,7 @@ import HomePageStyle from "./HomePage.module.css";
 function HomePage() {
   return (
     <h1 className={HomePageStyle.HomePage}> 
-      <p>Wecome To Influencer Management Portal</p>
+      <p>Welcome To Influencer Management Portal</p>
       <p>This site is under development</p>
     </h1>
   );
