f8d17b538a7bc5cd74095953b4320cea40a925fd
Arnab Biswas <abiswas@commvault.com>
2022-07-09 17:39:28 +0530

    CSS Fix

--- src/components/HomePage/HomPage.js
+++ src/components/HomePage/HomPage.js
@@ -3,7 +3,7 @@ import HomePageStyle from "./HomePage.module.css";
 function HomePage() {
     console.log(HomePageStyle);
   return (
-    <h1 className={HomePageStyle.HomePage}>
+    <h1 className={HomePageStyle.HomePage}> 
       <p>Wecome To Influencer Management Portal</p>
       <p>This site is under development</p>
     </h1>
